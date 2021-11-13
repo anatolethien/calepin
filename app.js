353df0abf6adc8df7026636410fd4c098249febb
anatolethien <anatole.thien@ynov.com>
2021-11-13 17:42:45 +0100

    Bulk

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ const express = require('express');
 
 const documents = require('./routes/documents');
 const static = require('./routes/static');
+const api = require('./routes/api');
 
 const app = express();
 const port = 3000;
 
 // middleware
+app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 // route middleware
 app.use('/', documents);
-app.use('/static', static);
+app.use('/', static);
+app.use('/api', api);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
--- routes/documents.js
+++ routes/documents.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const api = require('../api/api');
 
 const router = express.Router();
 
@@ -7,13 +8,34 @@ router.get('/', (req, res) => {
     res.status(200).render('index');
 });
 
+router.get('/recipes', (req, res) => {
+    console.log(api.length);
+    res.status(200).render('recipes', {
+        recipes: [
+            api[Math.floor(Math.random() * api.length)],
+            api[Math.floor(Math.random() * api.length)],
+            api[Math.floor(Math.random() * api.length)]
+        ]
+    });
+});
+
 router.get('/recipes/:recipe', (req, res) => {
-    // TODO: compare $recipe with item.name.toLowerCase().replace(/\s/g , "-")
-    res.send(req.params)
+    for (var id of api) {
+        if (id.name.toLowerCase().replace(/\s/g , "-") === req.params.recipe) {
+            res.status(200).render('recipe', {
+                name: id.name,
+                description: id.description,
+                image: id.image,
+                ingredients: id.ingredients,
+                directions: id.directions
+            });
+        }
+    }
+    res.status(404).render('error', {status: 404, error: 'Not Found'});
 });
 
 router.get('/api', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '../public/api.json'));
+    res.status(200).sendFile(path.join(__dirname, '../api/api.json'));
 });
 
 module.exports = router;
